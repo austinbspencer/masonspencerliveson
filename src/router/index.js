d82437352cb3aec654219fbd8bbf3131171edece
Austin Spencer <abspencer2097@gmail.com>
2021-05-06 12:08:00 -0700

    adding about austin

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,20 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
+    path: '/about/mason',
     name: 'About Mason',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutMason.vue')
+  },
+  {
+    path: '/about/austin',
+    name: 'About Austin',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutAustin.vue')
   },
   {
     path: '/donate',
---
+++ src/views/AboutAustin.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="about pa-5">
+    <v-row justify="space-around">
+      <v-col
+        cols="12"
+        sm="10"
+        md="6"
+        lg="6"
+        xl="6"
+      >
+        <austin-bio />
+      </v-col>
+      <v-col
+        cols="10"
+        md="6"
+        lg="6"
+        xl="6"
+      >
+        <carousel-card />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import AustinBio from "../components/About/AustinBio.vue";
+import CarouselCard from "../components/About/Carousel.vue";
+
+export default {
+  name: "Aboutaustin",
+  components: {
+    "austin-bio": AustinBio,
+    "carousel-card": CarouselCard,
+  },
+  directives: {},
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
