ec682bab05e29a2b6bac57c0460e90a1eafb665d
Austin Spencer <abspencer2097@gmail.com>
2021-05-05 23:06:39 -0700

    adding menu bar

---
+++ src/components/AppMenu.vue
@@ -0,0 +1,126 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      v-model="drawer"
+      color="background"
+      app
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title primary--text">
+            Menu
+          </v-list-item-title>
+          <!-- <v-list-item-subtitle>
+            </v-list-item-subtitle> -->
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      color="background"
+      app
+      hide-on-scroll
+    >
+      <v-app-bar-nav-icon
+        class="secondary--text d-md-none"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+
+      <v-toolbar-title class="secondary--text">{{ $route.name }}</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-toolbar-items
+        class="d-none d-md-flex"
+        v-for="item,i in items"
+        v-bind:key="i"
+      >
+        <v-btn
+          v-if="item.title !== $route.name"
+          class="ml-3 mr-3"
+          text
+          :to="item.to"
+        >
+          {{item.title}}
+        </v-btn>
+
+      </v-toolbar-items>
+      <v-btn
+        @click="darkMode()"
+        fab
+        elevation="1"
+        icon
+      >
+        <v-icon class="secondary--text">mdi-invert-colors</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <!-- Button that appears once you start scrolling on the page that will take you to the top -->
+    <v-fab-transition>
+      <v-btn
+        v-scroll="onScroll"
+        v-show="fab"
+        fab
+        dark
+        fixed
+        bottom
+        right
+        color="primary"
+        @click="toTop"
+      >
+        <v-icon>mdi-arrow-up-thick</v-icon>
+      </v-btn>
+    </v-fab-transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+  data: () => ({
+    drawer: false,
+    fab: false,
+  }),
+  methods: {
+    darkMode() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+    onScroll(e) {
+      if (typeof window === "undefined") return;
+      const top = window.pageYOffset || e.target.scrollTop || 0;
+      this.fab = top > 20;
+    },
+    toTop() {
+      this.$vuetify.goTo(0);
+    },
+  },
+  computed: {
+    items() {
+      return this.$store.getters.items;
+    },
+    projects() {
+      return this.$store.getters.projects;
+    },
+  },
+};
+</script>
