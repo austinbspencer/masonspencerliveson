98e32349b9a8dfe9056f6bf1ea3ba88b146aa84f
Austin Spencer <abspencer2097@gmail.com>
2021-05-05 23:06:30 -0700

    add routes

--- src/router/index.js
+++ src/router/index.js
@@ -12,11 +12,16 @@ const routes = [
   },
   {
     path: '/about',
-    name: 'About',
+    name: 'About Mason',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/donate',
+    name: 'Donate',
+    component: () => import(/* webpackChunkName: "donate" */ '../views/Donate.vue')
   }
 ]
 
