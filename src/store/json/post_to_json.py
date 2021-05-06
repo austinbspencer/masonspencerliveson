42a07d3aa2e75a0274664bcf8ccba9b9359ee7fd
Austin Spencer <abspencer2097@gmail.com>
2021-05-06 14:24:44 -0700

    now able to get all posts from folder in order

--- src/store/json/post_to_json.py
+++ src/store/json/post_to_json.py
@@ -11,7 +11,7 @@ class mydict(dict):
         return json.dumps(self)
 
 
-def linkFound(line):
+def linkFound(dict1, line):
     words = line.split()
     for word in words:
         if 'http' in word:
@@ -19,58 +19,71 @@ def linkFound(line):
                 "title": "some title",
                 "url": word
             })
+    return dict1
 
 
-# directory = 'posts'
-# for filename in os.listdir(directory):
-#     f = os.path.join(directory, filename)
-#     # checking if it is a file
-#     if os.path.isfile(f):
-#         print(f)
-
-# the file to be converted
-filename = 'post.txt'
-
-# resultant dictionary
-dict1 = {
-    "date": "",
-    "year": "",
-    "pfp": "pfp.jpeg",
-    "author": "Mason Spencer Lives On",
-    "url": "",
-    "links": [
-    ],
-    "message": [
-    ]
-}
-
-# fields in the sample file
-fields = ['name', 'designation', 'age', 'salary']
-
-with open(filename) as fh:
-
-    # count variable for employee id creation
-    l = 1
-
-    for line in fh:
-
-        if line[:4] == 'date':
-            dict1['date'] = line[5:].rstrip()
-
-        elif line[:4] == 'year':
-            dict1['year'] = line[5:].rstrip()
-
-        elif line[0].isalpha():
-            dict1['message'].append(
-                {'content': line.rstrip(), "type": "Paragraph"})
-            if 'http' in line:
-                linkFound(line)
-
-        else:
-            dict1['message'].append(
-                {'content': line[2:].rstrip(), "type": "Bullet"})
-            if 'http' in line:
-                linkFound(line)
+jsonList = []
 
 
-print(mydict(dict1))
+def makeJson(f):
+    # the file to be converted
+    filename = f
+
+    dict1 = {
+        "date": "",
+        "year": "",
+        "pfp": "pfp.jpeg",
+        "author": "Mason Spencer Lives On",
+        "url": "",
+        "attachments": None,
+        "links": [
+        ],
+        "message": [
+        ]
+    }
+
+    with open(filename) as fh:
+
+        for line in fh:
+
+            if line[:4] == 'date':
+                dict1['date'] = line[5:].rstrip()
+
+            elif line[:4] == 'year':
+                dict1['year'] = line[5:].rstrip()
+
+            elif line[0].isalpha():
+                dict1['message'].append(
+                    {'content': line.rstrip(), "type": "Paragraph"})
+                if 'http' in line:
+                    dict1 = linkFound(dict1, line)
+
+            else:
+                dict1['message'].append(
+                    {'content': line[2:].rstrip(), "type": "Bullet"})
+                if 'http' in line:
+                    dict1 = linkFound(dict1, line)
+
+        jsonList.append(dict1)
+
+
+def main():
+    directory = 'posts'
+    posts = 0
+    for _filename in os.listdir(directory):
+        # checking if it is a file
+        # if os.path.isfile(f):
+        #     makeJson(f)
+        posts += 1
+    i = 1
+    while i <= posts:
+        file_name = 'posts/post' + str(i) + '.txt'
+        makeJson(file_name)
+        i += 1
+
+
+if __name__ == '__main__':
+    main()
+    out_file = open("out.json", "w")
+    json.dump(jsonList, out_file, indent=4, sort_keys=False)
+    out_file.close()
