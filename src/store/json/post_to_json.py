f42ebfcc06e118ad1a03dd74c62c9074d6f79ac8
Austin Spencer <abspencer2097@gmail.com>
2021-05-07 10:10:16 -0700

    cleaning up python script and adding comments

--- src/store/json/post_to_json.py
+++ src/store/json/post_to_json.py
@@ -1,9 +1,15 @@
-# Python program to convert text
-# file to JSON
+'''
+Python program to convert files to JSON
+Specifically for use in masonspencerliveson webpage
+Built to take Facebook posts and create JSON for displaying in the webpage
 
+
+Austin Spencer - 05 / 06 / 2021
+'''
 
 import os
 import json
+import time
 
 
 class mydict(dict):
@@ -11,16 +17,17 @@ class mydict(dict):
         return json.dumps(self)
 
 
-jsonList = []
-links = 0
-
-
-def linkFound(dict1, line):
+def linkFound(dict1, line, links):
+    '''
+    Helper function that will be called when 'http' is found in the file line
+    This will assume the word with 'http' in it is a link and will replace the link with phrase below
+    Will also add to the dict1 with the link url and the title of the link based on how many links found
+    in the current file
+    '''
     words = line.split()
-    global links
     for word in words:
         if 'http' in word:
-            links += 1
+            links += 1  # increment links which keeps track of the number of links found in THIS file
             line = line.replace(
                 word, f'**The link is "Link {links}" at the bottom of this post!**')
             dict1['links'].append({
@@ -30,12 +37,16 @@ def linkFound(dict1, line):
     return dict1, line
 
 
-def makeJson(f):
-    # the file to be converted
-    filename = f
+def makeJson(filename):
+    '''
+    makeJson will take a file and iterate through it and append to / edit dict1 with the content of the file
+    Once completed iterating the entire file returns the updated dict1 dictionary
+
+    Param(filename) : name of file to be converted to dictionary
+    Return : dictionary of the files content
+    '''
 
     # Reset number of links
-    global links
     links = 0
 
     # After running no values should be left as default
@@ -56,7 +67,7 @@ def makeJson(f):
 
         for line in fh:
 
-            # Check for empty line
+            # Check for empty line and ignore the line if it's empty
             if line != "\n":
 
                 # Add the date
@@ -78,39 +89,67 @@ def makeJson(f):
                 # Add paragraph content
                 elif line[0].isalpha():
                     if 'http' in line:
-                        dict1, line = linkFound(dict1, line)
+                        dict1, line = linkFound(dict1, line, links)
                     dict1['message'].append(
                         {'content': line.rstrip(), "type": "Paragraph"})
 
                 # Add bullet point content
                 else:
                     if 'http' in line:
-                        dict1, line = linkFound(dict1, line)
+                        dict1, line = linkFound(dict1, line, links)
                     dict1['message'].append(
                         {'content': line[2:].rstrip(), "type": "Bullet"})
-            else:
-                print('empty line ignored')
 
-        jsonList.append(dict1)
+        # Close file after finished for best practice
+        fh.close()
+
+        # Return the updated dictionary
+        return dict1
 
 
 def main():
+    # Initializing variables
+    json_list = []
     directory = 'posts'
     posts = 0
+
+    # Get total number of posts
     for _filename in os.listdir(directory):
         # checking if it is a file
         # if os.path.isfile(f):
         #     makeJson(f)
         posts += 1
     i = 1
+
+    # Iterate through all posts
     while i <= posts:
         file_name = 'posts/post' + str(i) + '.txt'
-        makeJson(file_name)
+
+        # Call helper function with bulk of the algorithm
+        json_list.append(makeJson(file_name))
+
+        # Output how far along the program is
+        # Completely unecessary and the sleep is needed because it builds so fast
+        if i % 5 == 0:
+            pct = float(i / posts) * 100
+            print(f'--> {pct}%')
+            time.sleep(1)
         i += 1
+
+    # Output 100% complete if the total posts isn't % 5
+    if pct != 100.0:
+        print('--> 100.0%')
+
+    return json_list
 
 
 if __name__ == '__main__':
-    main()
+    json_list = main()
+
     out_file = open("out.json", "w")
-    json.dump(jsonList, out_file, indent=4, sort_keys=False)
+
+    # Dump the json into the new file without sort
+    json.dump(json_list, out_file, indent=4, sort_keys=False)
+
+    # Close the file for best practice
     out_file.close()
